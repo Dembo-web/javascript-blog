ebb0597e4d225f5866ea37823c2cb4319b0b5960
Debowski Jakub <karvis95@gmail.com>
2020-12-14 23:30:28 +0100

    Add finding and showing articles

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@
 
 const titleClickHandler = function(event){
 
+  event.preventDefault();
+
   const clickedElement = this;
 
   console.log('Link was clicked!');
@@ -19,7 +21,7 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
     console.log('clickedElement (with plus): ' + clickedElement);
     clickedElement.classList.add('active');
@@ -32,11 +34,20 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
-  /* add class 'active' to the correct article */
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
+
+  /* [DONE] add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+
 }
 
 const links = document.querySelectorAll('.titles a');
